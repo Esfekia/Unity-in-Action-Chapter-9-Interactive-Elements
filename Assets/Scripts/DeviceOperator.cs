8448fe7ebbd5fd3f2e53d3ccd720fc31e955c9d3
Esfekia <sertac@gmail.com>
2022-07-01 09:23:23 -0400

    use dot product of vector3's (player and object) to check facing. Now door only opens if player is looking in general direction of the door.
    use dot product of vector3's (player and object) to check facing. Now door only opens if player is looking in general direction of the door.

--- Assets/Scripts/DeviceOperator.cs
+++ Assets/Scripts/DeviceOperator.cs
@@ -15,9 +15,23 @@ public class DeviceOperator : MonoBehaviour
             // OverlapSphere() returns a list of nearby objects
             Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius);
             foreach (Collider hitCollider in hitColliders)
-            {
-                // SendMessage() tries to call the named function, regardless of the target's type
-                hitCollider.SendMessage("Operate", SendMessageOptions.DontRequireReceiver);
+            {                
+                Vector3 hitPosition = hitCollider.transform.position;
+
+                // vertical correction so the direction won't point up or down
+                hitPosition.y = transform.position.y;
+
+                // make a direction vector by subtracting player pos from object pos
+                Vector3 direction = hitPosition - transform.position;
+                
+                // close to 1f means looking at the same direction.
+                if (Vector3.Dot(transform.forward, direction.normalized) > 0.5f)
+                {
+                    // SendMessage() tries to call the named function, regardless of the target's type
+                    hitCollider.SendMessage("Operate", SendMessageOptions.DontRequireReceiver);
+                }
+
+
             }
         }
     }
