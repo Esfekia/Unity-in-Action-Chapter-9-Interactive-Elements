de1e503f3efdbae3db25125b206982db67b2a2aa
Esfekia <sertac@gmail.com>
2022-07-02 07:20:34 -0400

    InventoryManager script inititated, PlayerManager script also initiated, with health and maxHealth values set.
    InventoryManager script inititated, PlayerManager script also initiated, with health and maxHealth values set.

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour, IGameManager
+{
+    // property can be read from anywhere but set only within this script
+    public ManagerStatus status { get; private set; }
+        
+    public void Startup()
+    {
+        // any long-running startup tasks go here
+        Debug.Log("Inventory manager starting...");        
+        
+        // for long-running tasks, use status "Initialized" instead, once finished, then "Started"
+        status = ManagerStatus.Started;
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour, IGameManager
+{
+    // status can be read from anywhere (getter is public), but set only within this script (the setter is private)
+    public ManagerStatus status { get; private set; }
+
+    public int health { get; private set; }
+    public int maxHealth { get; private set; }
+
+    public void Startup()
+    {
+        Debug.Log("Player manager starting...");
+
+        // these values could be initialized with saved data
+        health = 50;
+        maxHealth = 100;
+
+        // change status to started after only everything loads/finishes, otherwise it is "Initialized"
+        status = ManagerStatus.Started;
+    }
+
+    // other scripts cannot set health directly, but can call this function
+    public void ChangeHealth(int value)
+    {
+        health += value;
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+        else if (health < 0)
+        {
+            health = 0;
+        }
+        Debug.Log($"Health: {health} / {maxHealth}");
+    }
+
+}
