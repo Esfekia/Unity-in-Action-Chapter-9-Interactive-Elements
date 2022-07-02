366990668cdfc22ac5e8482bed252275c1341dab
Esfekia <sertac@gmail.com>
2022-07-01 20:55:32 -0400

    Added energy, ore, health and key collectibles with collectibleItem scripts attached
    Added energy, ore, health and key collectibles with collectibleItem scripts attached

--- Assets/Scripts/CollectibleItem.cs
+++ Assets/Scripts/CollectibleItem.cs
@@ -10,6 +10,9 @@ public class CollectibleItem : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log($"Item collected: {itemName}");
+        
+        // this referes only to this script component
+        // this.gameObject refers to the object the script is attached to
         Destroy(this.gameObject);
     }
 }
