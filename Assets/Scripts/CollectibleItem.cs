f293b2faaf21cb5014e8b488930561620f37e4b7
Sertac <sertac@gmail.com>
2022-08-22 09:59:57 -0400

    Updated InventoryManager with a public AddItem module and CollectibleItem now calls this module to add an item to the inventory list before trigger-destroying it.
    Updated InventoryManager with a public AddItem module and CollectibleItem now calls this module to add an item to the inventory list before trigger-destroying it.

--- Assets/Scripts/CollectibleItem.cs
+++ Assets/Scripts/CollectibleItem.cs
@@ -9,10 +9,15 @@ public class CollectibleItem : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log($"Item collected: {itemName}");
+        // instead of just destroying the object upon trigger, let us first add it to our inventory
+        Managers.Inventory.AddItem(itemName);
+        Destroy(this.gameObject);
         
+        // Debug.Log($"Item collected: {itemName}");
         // this referes only to this script component
         // this.gameObject refers to the object the script is attached to
-        Destroy(this.gameObject);
+        // Destroy(this.gameObject);
+       
+
     }
 }
--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -6,15 +6,38 @@ public class InventoryManager : MonoBehaviour, IGameManager
 {
     // property can be read from anywhere but set only within this script
     public ManagerStatus status { get; private set; }
-        
+
+    private List<string> items;
     public void Startup()
     {
         // any long-running startup tasks go here
-        Debug.Log("Inventory manager starting...");        
-        
+        Debug.Log("Inventory manager starting...");
+
+        // initialize the empty item list
+        items = new List<string>();
+
         // for long-running tasks, use status "Initialized" instead:
-        
+
         // once finished, then "Started"
         status = ManagerStatus.Started;
+    }
+
+    // print console message of the current inventory
+    public void DisplayItems()
+    {
+        string itemDisplay = "Items: ";
+        foreach(string item in items)
+        {
+            itemDisplay += item + " ";
+        }
+        Debug.Log(itemDisplay);
+    }
+
+    // other scripts can't manipulate the item list directly but can call this.
+    public void AddItem(string name)
+    {
+        items.Add(name);
+
+        DisplayItems();
     }
 }
